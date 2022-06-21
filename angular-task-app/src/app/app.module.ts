6f65d4ec7f479cf6a68522c58025d7b27a81efdb
panvla <pandvlado@gmail.com>
2022-06-21 09:35:41 +0200

    done

--- angular-task-app/src/app/app.module.ts
+++ angular-task-app/src/app/app.module.ts
@@ -8,6 +8,16 @@ import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+
+const appRouter: Routes = [
+  
+    {path:'', component: TasksComponent},
+    {path:'about', component: AboutComponent}
+];
 
 @NgModule({
   declarations: [
@@ -15,12 +25,16 @@ import { HttpClientModule } from '@angular/common/http';
     HeaderComponent,
     ButtonComponent,
     TasksComponent,
-    TaskItemComponent
+    TaskItemComponent,
+    AddTaskComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRouter)
   ],
   providers: [],
   bootstrap: [AppComponent]
